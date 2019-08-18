435b262f4ad10ac6727d168fd68734cf4ee0d406
Heather Etheridge <heathernetheridge@gmail.com>
2019-08-18 18:25:14 -0500

    added a route for show.ejs

--- routes/flights.js
+++ routes/flights.js
@@ -5,6 +5,7 @@ var flightsCtrl = require('../controllers/flights');
 //Routes for Form views  Flights
 router.get('/', flightsCtrl.indexView);
 router.get('/new', flightsCtrl.newView);
+router.get('/:id', flightsCtrl.showView);
 
 //Routes for Form actions
 router.post('/', flightsCtrl.create);
