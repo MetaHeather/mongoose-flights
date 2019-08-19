f4250b147aa0a7d10bf327b62497e9537de45410
Heather Etheridge <heathernetheridge@gmail.com>
2019-08-19 16:51:33 -0500

    Fixed 404 error showing when GET details page

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,42 +1,47 @@
 var Flight = require('../models/flight');
 
 module.exports = {
-    indexView,
-    newView,
-    create,
-    showView
-  };
+  indexView,
+  newView,
+  create,
+  showView
+};
 
-  //functione that return views
-  function indexView(req, res){
-  Flight.find({}, function(err, flights) {
-    res.render('flights/index', { title: 'All Flights', flights});
-  })
-  };
- 
- //when GET new redirects to new.ejs 
+//functione that return views
+function indexView(req, res) {
+  Flight.find({}, function (err, flights) {
+    res.render('flights/index', {
+      title: 'All Flights',
+      flights
+    });
+  });
+};
+
+//when GET new redirects to new.ejs 
 function newView(req, res) {
-  res.render('flights/new', {title: 'Add Flight'});
+  res.render('flights/new', {
+    title: 'Add Flight'
+  });
 }
 
 
-  function create(req, res){ 
-        for (let key in req.body) {
-      if (req.body[key] === '') delete req.body[key];
-    }
-    var flight = new Flight(req.body);
-    flight.save(function(err) {
-      if (err) return res.redirect('/flights/new');
-      res.redirect('/flights');
-    });
-  }; 
-
-  //function for showing details page
-  function showView(req, res) {
-    Flight.findById(req.params.id, function (err, flight) {
-      res.render('flights/show', {
-        title: 'Flight Details',
-        flight
-      })
-    })
+function create(req, res) {
+  for (let key in req.body) {
+    if (req.body[key] === '') delete req.body[key];
   }
+  var flight = new Flight(req.body);
+  flight.save(function (err) {
+    if (err) return res.redirect('/flights/new');
+    res.redirect('/flights');
+  });
+};
+
+//function for showing details page
+function showView(req, res) {
+  Flight.findById(req.params.id, function (err, flight) {
+    res.render('flights/show', {
+      title: 'Flight Details',
+      flight
+    });
+  });
+};
