2e02cea379682807cbb5906c2c8989b8a1aa63e8
Heather Etheridge <heathernetheridge@gmail.com>
2019-08-18 23:16:02 -0500

    Added routes for destinations

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ require('./config/database');
 //Import routers
 var indexRouter = require('./routes/index');
 var flightsRouter = require('./routes/flights');
+var destinationsRouter = require('./routes/destinations');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //set up router files as middleware
 app.use('/', indexRouter);
 app.use('/flights', flightsRouter);
+app.use('/', destinationsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
