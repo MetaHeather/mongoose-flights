f4e26948d6e0a02ab332ed279931db698dbceeac
Heather Etheridge <heathernetheridge@gmail.com>
2019-08-20 00:17:14 -0500

    Added create() for ticketCtrl

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,9 +1,13 @@
 var Ticket = require('../models/ticket');
+var Flight = require('../models/flight');
 
 module.exports = {
-    newTicket
+    newTicket,
+    create 
 };
 
+
+//Rerouts to add ticket form 
 function newTicket(req, res) {
     console.log("new ticket route");
     res.render('tickets/new',{ 
@@ -11,3 +15,17 @@ function newTicket(req, res) {
         fId: req.params.id 
     });
 };
+
+//creates a new ticket
+function create(req, res) {
+    let ticket = new Ticket({
+        seat: req.body.seat,
+        price: req.body.price,
+        flight: req.params.id
+    });
+    ticket.save(function (err) {
+        if (err) return res.redirect(`flights/${req.params.id}/tickets/new`);
+        res.redirect(`/flights/${req.params.id}`);
+    });
+};
+
--- routes/tickets.js
+++ routes/tickets.js
@@ -4,5 +4,7 @@ var router = express.Router();
 var ticketsCtrl = require('../controllers/tickets');
 
 router.get('/flights/:id/tickets/new', ticketsCtrl.newTicket);
+//router for posting ticket to show ejs and saving to flight
+router.post('/flights/:id/tickets', ticketsCtrl.create);
 
 module.exports = router;
